dab667a641d0d1b2bc02dffc08b3493a2ce3c9d8
sdasd30 <danielzhao2@gmail.com>
2021-12-26 19:21:36 -0500

    Stuff for map design

---
+++ Assets/Scripts/Lighting/TilemapShadowcaster.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine;
+
+public class TilemapShadowcaster : MonoBehaviour
+{
+    CompositeCollider2D tilemapCollider;
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        tilemapCollider = GetComponent<CompositeCollider2D>();
+        GameObject shadowCasterContainer = GameObject.Find("shadow_casters");
+        for (int i = 0; i < tilemapCollider.pathCount; i++)
+        {
+            Vector2[] pathVertices = new Vector2[tilemapCollider.GetPathPointCount(i)];
+            tilemapCollider.GetPath(i, pathVertices);
+            GameObject shadowCaster = new GameObject("shadow_caster_" + i);
+            PolygonCollider2D shadowPolygon = (PolygonCollider2D)shadowCaster.AddComponent(typeof(PolygonCollider2D));
+            shadowCaster.transform.parent = shadowCasterContainer.transform;
+            shadowPolygon.points = pathVertices;
+            shadowPolygon.enabled = false;
+            ShadowCaster2D shadowCasterComponent = shadowCaster.AddComponent<ShadowCaster2D>();
+            shadowCasterComponent.selfShadows = true;
+        }
+    }
+
+    private Vector3 vec2To3(Vector2 inputVector)
+    {
+        return new Vector3(inputVector.x, inputVector.y, 0);
+    }
+
+}
