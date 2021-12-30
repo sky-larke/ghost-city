cd9ce045ea491d4cbf1bccb223108e6eea96cac8
Richard Bann <richard@bann.com>
2021-12-30 17:45:20 -0500

    Health bar object
    Created a health bar object that can be used for any game object

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Going to change depending on what hurts the game object
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+}
